e43e0c749986dcadf48ee2fc1f8daed39b556f5d
Al <lin.alice72@gmail.com>
2018-03-01 02:04:28 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
+
+  data.each do |traits, traitset|
+    traitset.each do |set, names|
+        names.each do |name|
+          if result[name].nil?
+            result[name] = {}
+          end
+          if result[name][traits].nil?
+            result[name][traits] = []
+          end
+          result[name][traits] << set.to_s
+      end
+    end
+  end
+result
 end
