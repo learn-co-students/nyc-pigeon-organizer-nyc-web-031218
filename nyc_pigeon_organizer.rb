d47ebf99c8149cafb519b6b82a63c3c690de01be
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-03-07 02:43:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,27 +3,38 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   names = data[:color].values.flatten.uniq
-  pigeonhash= {}
   ans = {}
 
-  data.keys.each do |attribute|
-    pigeonhash[attribute] = []
-  end
 
   names.each do |name|
-    ans[name] = pigeonhash
+    ans[name] = {}
+    data.keys.each do |attribute|
+      ans[name].merge!({attribute => []})
+    end
   end
 
-  data.each do |description, hash|
+    data.each do |description, hash|
     hash.each do |attribute, namearr|
-      namearr.each do |name|
-        if attribute.is_a?(Symbol) && !(ans[name][description].include?(attribute.id2name))
-          ans[name][description] << attribute.id2name
-        elsif attribute.is_a?(String) && !(ans[name][description].include?(attribute))
-          ans[name][description] << attribute
+      namearr.each do |nam|
+        if attribute.is_a?(Symbol)
+          ans[nam][description] << attribute.id2name
+        else
+          ans[nam][description] << attribute
         end
       end
     end
   end
   return ans
+
 end
+
+#         if attribute.is_a?(Symbol) && !(ans[name][description].include?(attribute))
+#           ans[name][description] << attribute.id2name
+#         elsif attribute.is_a?(String) && !(ans[name][description].include?(attribute))
+#           ans[name][description] << attribute
+#         end
+#       end
+#     end
+#   end
+#   return ans
+# end
