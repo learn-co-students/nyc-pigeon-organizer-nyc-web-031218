47e81429e6cbbb1f410027311005ad9a75173069
adamtowers <hello@adamtowers.design>
2018-03-08 18:46:56 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+
+  data.each do |key, value|
+    value.each do |key2, value2_array|
+      value2_array.each do |name|
+        if !new_hash.include?(name)
+          new_hash[name] = {key => [key2.to_s]}
+        elsif new_hash[name].include?(key)
+          new_hash[name][key] << key2.to_s
+        else
+          new_hash[name][key] = [key2.to_s]
+        end
+      end
+    end
+  end
+
+  new_hash
 end
